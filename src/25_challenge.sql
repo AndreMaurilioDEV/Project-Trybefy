fa70da9642c06f1a8a05f518ed546b922bbb99aa
André Maurilio <andreandrade1920@gmail.com>
2024-02-28 20:31:02 -0300

    req 25

--- src/25_challenge.sql
+++ src/25_challenge.sql
@@ -1,7 +1,9 @@
 
-SELECT album.title AS 'Álbum', COUNT(song.album_id) AS 'Quantidade de músicas reproduzidas' FROM `Trybefy`.albums AS album
+SELECT album.title AS 'Álbum', COUNT(historySong.id) AS 'Quantidade de músicas reproduzidas' FROM `Trybefy`.albums AS album
 INNER JOIN `Trybefy`.songs AS song 
 ON album.id = song.album_id
+INNER JOIN `Trybefy`.history_play_songs AS historySong
+ON song.id = historySong.song_id
 GROUP BY album.title
-ORDER BY COUNT(song.album_id) DESC
+ORDER BY COUNT(historySong.id) DESC
 LIMIT 5;
